12858f270e3484773ff2cdd4398fd161c81f38f3
pedro.henrique <pedro.henrique@brunsker.com.br>
2023-07-25 12:49:31 -0300

    Feat: toolbar service

--- libs/core/ui/dynamic-table/src/lib/core-ui-dynamic-table.module.ts
+++ libs/core/ui/dynamic-table/src/lib/core-ui-dynamic-table.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HelloWordComponent } from './components/hello-word/hello-word.component';
 import { TuiTagModule } from '@taiga-ui/kit';
-
 @NgModule({
   imports: [CommonModule, TuiTagModule],
   declarations: [HelloWordComponent],
   exports: [HelloWordComponent],
 })
+
 export class CoreUiDynamicTableModule {}
--- libs/core/ui/toolbar/src/lib/components/toolbar/toolbar.component.ts
+++ libs/core/ui/toolbar/src/lib/components/toolbar/toolbar.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { BehaviorSubject } from 'rxjs';
+import { ToolbarService } from '../../services/toolbar/toolbar.service';
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.scss']
+  styleUrls: ['./toolbar.component.scss'],
 })
 export class ToolbarComponent implements OnInit {
+  menuEvent$: BehaviorSubject<boolean>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private readonly ToolbarService: ToolbarService) {
+    this.menuEvent$ = this.ToolbarService.menuEvent$;
   }
 
+  ngOnInit() {
+    this.menuEvent$.subscribe(console.log);
+  }
+
+  emitEvent() {
+    this.menuEvent$.next(!this.menuEvent$.value);
+  }
 }
