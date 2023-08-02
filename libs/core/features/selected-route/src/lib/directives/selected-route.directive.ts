d0f8267060156cb7734eef31d2d512d448bb8a52
pedroping <pedrpchavesdias@gmail.com>
2023-08-01 23:49:27 -0300

    Feat: Add directive callback function

--- libs/core/features/selected-route/src/lib/directives/selected-route.directive.ts
+++ libs/core/features/selected-route/src/lib/directives/selected-route.directive.ts
@@ -1,13 +1,18 @@
-import { Directive, HostBinding, Input } from '@angular/core';
+import { Directive, HostBinding, HostListener, Input } from '@angular/core';
 import { Router } from '@angular/router';
 @Directive({
   selector: '[appSelectedRoute]',
 })
 export class SelectedRouteDirective {
   @Input({ required: true }) activedRoute?: string;
+  @Input() clickFunction?: Function;
 
   @HostBinding('class.selected') get validate() {
     return this.activedRoute && this.route.url.includes(this.activedRoute);
+  }
+
+  @HostListener('click') onClick() {
+    if (this.clickFunction) this.clickFunction();
   }
 
   constructor(private readonly route: Router) {}
