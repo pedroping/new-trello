3466ecc88f9daeafed37c792708d1495bed074b5
pedro.henrique <pedro.henrique@brunsker.com.br>
2023-08-09 10:39:57 -0300

    Feat: Table-mocks

---
+++ apps/default-app/src/app/domain/dynamic-table/helpers/table-mocks.ts
@@ -0,0 +1,41 @@
+import { ITableColumn } from "@my-monorepo/core/ui/dynamic-table"
+
+export const COLUMNS: ITableColumn[] = [
+    {
+      label: 'Nome',
+      selector: 'name'
+    },
+    {
+      label: 'Idade',
+      selector: 'age'
+    },
+    {
+      label: 'E-mail',
+      selector: 'gmail'
+    }
+  ]
+  
+  
+  export const DATA = [
+    {
+      name: 'Pedro',
+      age: 12,
+      gmail: 'pedrofofao2014@gmial.com'
+    },
+    {
+      name: 'Pedro',
+      age: 12,
+      gmail: 'pedrofofao2014@gmial.com'
+    },
+    {
+      name: 'Pedro',
+      age: 12,
+      gmail: 'pedrofofao2014@gmial.com'
+    },
+    {
+      name: 'Pedro',
+      age: 12,
+      gmail: 'pedrofofao2014@gmial.com'
+    }
+  ]
+  
--- apps/default-app/src/app/domain/dynamic-table/pages/dynamic-table/dynamic-table.component.ts
+++ apps/default-app/src/app/domain/dynamic-table/pages/dynamic-table/dynamic-table.component.ts
@@ -1,48 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CoreUiDynamicTableModule } from '@my-monorepo/core/ui/dynamic-table'
-import { ITableColumn } from '@my-monorepo/core/ui/dynamic-table'
-
-export const COLUMNS: ITableColumn[] = [
-  {
-    label: 'Nome',
-    selector: 'name'
-  },
-  {
-    label: 'Idade',
-    selector: 'age'
-  },
-  {
-    label: 'E-mail',
-    selector: 'gmail'
-  }
-]
-
-
-export const DATA = [
-  {
-    name: 'Pedro',
-    age: 12,
-    gmail: 'pedrofofao2014@gmial.com'
-  },
-  {
-    name: 'Pedro',
-    age: 12,
-    gmail: 'pedrofofao2014@gmial.com'
-  },
-  {
-    name: 'Pedro',
-    age: 12,
-    gmail: 'pedrofofao2014@gmial.com'
-  },
-  {
-    name: 'Pedro',
-    age: 12,
-    gmail: 'pedrofofao2014@gmial.com'
-  }
-]
-
-
-
+import { COLUMNS, DATA } from '../../helpers/table-mocks';
 @Component({
   selector: 'app-dynamic-table',
   templateUrl: './dynamic-table.component.html',
