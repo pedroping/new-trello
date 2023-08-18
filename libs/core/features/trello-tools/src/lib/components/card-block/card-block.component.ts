45e27b73053ad16692f96d30aaaf34fc9befbef5
pedroping <pedrpchavesdias@gmail.com>
2023-08-17 21:29:15 -0300

    Feat: trello block

---
+++ libs/core/features/trello-tools/src/lib/components/card-block/card-block.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'trello-card-block',
+  templateUrl: './card-block.component.html',
+  styleUrls: ['./card-block.component.scss'],
+})
+export class CardBlockComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
