0fc7282634a84920e8f0f872f62f62e179b4052b
pedroping <pedrpchavesdias@gmail.com>
2023-08-28 21:57:54 -0300

    Feat: expand table directive

--- apps/default-app/src/app/domain/dynamic-table/helpers/table-mocks.ts
+++ apps/default-app/src/app/domain/dynamic-table/helpers/table-mocks.ts
@@ -1,37 +1,37 @@
-import { ITableColumn, ITableConfig } from "@my-monorepo/core/ui/dynamic-table"
-import { BasicInputComponent } from "../../../core/components/basic-input/basic-input.component"
+import { ITableColumn, ITableConfig } from '@my-monorepo/core/ui/dynamic-table';
+import { BasicInputComponent } from '../../../core/components/basic-input/basic-input.component';
 
 const COLUMNS: ITableColumn<unknown>[] = [
   {
     label: 'Nome',
-    selector: 'name'
+    selector: 'name',
   },
   {
     label: 'Idade',
     selector: 'age',
     hasCustomField: true,
-    component: BasicInputComponent
+    component: BasicInputComponent,
   },
   {
     label: 'E-mail',
-    selector: 'gmail'
-  }
-]
+    selector: 'gmail',
+  },
+];
 
 export const DATA = Array.from({ length: 10 }, (_, index) => {
   return {
     name: 'Pedro ' + index,
     age: index + 10,
-    gmail: 'pedrofofao2014@gmial.com'
+    gmail: 'pedrofofao2014@gmial.com',
   };
-})
+});
 
 export const TABLE_CONFIG: ITableConfig<unknown> = {
   hasExpansion: false,
   hasPaginator: false,
   paginatorOptions: {
     pageSize: 2,
-    pageSizeOptions: [2, 10, 50, 100]
+    pageSizeOptions: [2, 10, 50, 100],
   },
   hasDefaultPaginator: true,
   defaultPaginatorOptions: {
@@ -39,8 +39,8 @@ export const TABLE_CONFIG: ITableConfig<unknown> = {
     currentPage: 1,
     pageSize: 2,
     previousLabel: 'Anterior',
-    nextLabel: 'Proximo'
+    nextLabel: 'Proximo',
   },
-  columns: COLUMNS
-}
-
+  columns: COLUMNS,
+  hasExpand: true,
+};
--- libs/core/ui/dynamic-table/src/lib/models/table.ts
+++ libs/core/ui/dynamic-table/src/lib/models/table.ts
@@ -1,35 +1,36 @@
-import { Type } from "@angular/core"
-import { FormControl } from "@angular/forms"
+import { Type } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 export interface ITableColumn<T> {
-    label: string
-    selector: string
-    hasCustomField?: boolean
-    component?: Type<DefaultInput<T>>
+  label: string;
+  selector: string;
+  hasCustomField?: boolean;
+  component?: Type<DefaultInput<T>>;
 }
 
 export interface ITableConfig<T> {
-    columns: ITableColumn<T>[]
-    hasExpansion: boolean
-    hasPaginator: boolean;
-    paginatorOptions?: IPaginatorOptions
-    hasDefaultPaginator?: boolean
-    defaultPaginatorOptions?: IDefaultPaginatorOptions
+  columns: ITableColumn<T>[];
+  hasExpansion: boolean;
+  hasPaginator: boolean;
+  paginatorOptions?: IPaginatorOptions;
+  hasDefaultPaginator?: boolean;
+  defaultPaginatorOptions?: IDefaultPaginatorOptions;
+  hasExpand: boolean;
 }
 export interface IPaginatorOptions {
-    pageSize: number
-    pageSizeOptions: number[]
+  pageSize: number;
+  pageSizeOptions: number[];
 }
 
 export interface IDefaultPaginatorOptions {
-    pageSize: number,
-    totalSize: number,
-    currentPage: number,
-    previousLabel: string,
-    nextLabel: string
+  pageSize: number;
+  totalSize: number;
+  currentPage: number;
+  previousLabel: string;
+  nextLabel: string;
 }
 export interface DefaultInput<T> {
-    tableElement: T
-    selector: keyof T
-    formControl: FormControl
+  tableElement: T;
+  selector: keyof T;
+  formControl: FormControl;
 }
