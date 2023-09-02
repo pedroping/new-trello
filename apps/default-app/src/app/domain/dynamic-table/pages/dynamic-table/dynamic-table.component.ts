22abea05fa456b3f259afe3d6928121da4375b63
pedroping <pedrpchavesdias@gmail.com>
2023-09-01 22:17:25 -0300

    Feat: form error

--- apps/default-app/src/app/domain/dynamic-table/pages/dynamic-table/dynamic-table.component.ts
+++ apps/default-app/src/app/domain/dynamic-table/pages/dynamic-table/dynamic-table.component.ts
@@ -1,5 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+} from '@angular/core';
 import {
   FormControl,
   FormGroup,
@@ -11,10 +16,14 @@ import {
   CoreFeaturesDynamicFormsModule,
   IInputBuilder,
 } from '@my-monorepo/core/features/dynamic-forms';
-import { CoreUiDynamicTableModule, IBaseTableFather, IBasicTableTest } from '@my-monorepo/core/ui/dynamic-table';
+import {
+  CoreUiDynamicTableModule,
+  IBaseTableFather,
+  IBasicTableTest,
+} from '@my-monorepo/core/ui/dynamic-table';
 import { BehaviorSubject, Observable, pipe, startWith, tap } from 'rxjs';
 import { CREATE_TABLE_CONFIG, DATA } from '../../helpers/table-mocks';
-import { CoreFeaturesFormErrorModule } from "@my-monorepo/core/features/form-error"
+import { CoreFeaturesFormErrorModule } from '@my-monorepo/core/features/form-error';
 @Component({
   selector: 'app-dynamic-table',
   templateUrl: './dynamic-table.component.html',
@@ -26,16 +35,18 @@ import { CoreFeaturesFormErrorModule } from "@my-monorepo/core/features/form-err
     CoreFeaturesDynamicFormsModule,
     FormsModule,
     ReactiveFormsModule,
-    CoreFeaturesFormErrorModule
+    CoreFeaturesFormErrorModule,
   ],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DynamicTableComponent implements OnInit, IBaseTableFather<IBasicTableTest> {
+export class DynamicTableComponent
+  implements OnInit, IBaseTableFather<IBasicTableTest>
+{
   tableConfig = CREATE_TABLE_CONFIG(this);
   data$ = new BehaviorSubject<IBasicTableTest[]>(DATA);
   DATA = DATA;
   form = new FormGroup({
-    teste: new FormControl('aaa', Validators.required),
+    teste: new FormControl(null as any, Validators.required),
   });
 
   formsConfig: IInputBuilder<unknown>[] = [
@@ -50,31 +61,45 @@ export class DynamicTableComponent implements OnInit, IBaseTableFather<IBasicTab
     },
   ];
 
-  constructor(private readonly cdr: ChangeDetectorRef) { }
+  constructor(private readonly cdr: ChangeDetectorRef) {}
 
   ngOnInit() {
-    if (this.tableConfig.customPagination)
-      this.customPagination()
+    if (this.tableConfig.customPagination) this.customPagination();
   }
 
-  getValueChanges(valueChanges$: Observable<IBasicTableTest>, id: number, element: IBasicTableTest, selector: keyof IBasicTableTest) {
+  getValueChanges(
+    valueChanges$: Observable<IBasicTableTest>,
+    id: number,
+    element: IBasicTableTest,
+    selector: keyof IBasicTableTest
+  ) {
     valueChanges$.pipe(startWith(element[selector])).subscribe((value) => {
-      console.log(`${id}- ${selector.toUpperCase()} Mudou para ${value} elemento`, element, this.findControl(selector, id));
-    })
+      console.log(
+        `${id}- ${selector.toUpperCase()} Mudou para ${value} elemento`,
+        element,
+        this.findControl(selector, id)
+      );
+    });
   }
 
   findControl(selector: keyof IBasicTableTest, id: number) {
-    const column = this.tableConfig.columns.find(column => column.selector == selector)
-    if (!column) return null
-    return column.controlsOptions?.controls[id]
+    const column = this.tableConfig.columns.find(
+      (column) => column.selector == selector
+    );
+    if (!column) return null;
+    return column.controlsOptions?.controls[id];
   }
 
   customPagination() {
     if (this.tableConfig.defaultPaginatorOptions) {
-      const start = this.tableConfig.defaultPaginatorOptions.pageSize * (this.tableConfig.defaultPaginatorOptions.currentPage - 1)
-      const end = this.tableConfig.defaultPaginatorOptions.pageSize * this.tableConfig.defaultPaginatorOptions.currentPage
-      const newData = this.DATA.slice(start, end)
-      this.data$.next(newData)
+      const start =
+        this.tableConfig.defaultPaginatorOptions.pageSize *
+        (this.tableConfig.defaultPaginatorOptions.currentPage - 1);
+      const end =
+        this.tableConfig.defaultPaginatorOptions.pageSize *
+        this.tableConfig.defaultPaginatorOptions.currentPage;
+      const newData = this.DATA.slice(start, end);
+      this.data$.next(newData);
       this.cdr.detectChanges();
     }
   }
@@ -82,8 +107,8 @@ export class DynamicTableComponent implements OnInit, IBaseTableFather<IBasicTab
   cdrTap() {
     return pipe(
       tap(() => {
-        this.cdr.detectChanges()
+        this.cdr.detectChanges();
       })
-    )
+    );
   }
 }
--- libs/core/features/form-error/src/lib/directives/form-error.directive.ts
+++ libs/core/features/form-error/src/lib/directives/form-error.directive.ts
@@ -1,41 +1,53 @@
-import { Directive, ElementRef, HostBinding, Input, OnInit, Optional, ViewContainerRef } from '@angular/core';
-import { AbstractControl, ControlContainer, FormControl, ValidationErrors } from '@angular/forms';
+import { Directive, HostBinding, Input, OnInit, Optional } from '@angular/core';
+import {
+  AbstractControl,
+  ControlContainer,
+  FormControl,
+  ValidationErrors,
+} from '@angular/forms';
 import { startWith } from 'rxjs';
+import { ERROR_MESSAGES } from '../models/erro-messages';
 
 @Directive({
-  selector: '[formError]'
+  selector: '[formError]',
 })
 export class FormErrorDirective implements OnInit {
+  @HostBinding('innerHTML') innerHtml = '';
 
-  @HostBinding('innerHTML') innerHtml = 'aaaaaaa'
-
-  @Input() formErrorControlName?: string
-  @Input() formErrorControl?: FormControl | AbstractControl
-
+  @Input() formErrorControlName?: string;
+  @Input() formErrorControl?: FormControl | AbstractControl;
 
   constructor(
-    @Optional() private readonly controlContainer: ControlContainer,
-  ) { }
+    @Optional() private readonly controlContainer: ControlContainer
+  ) {}
 
   ngOnInit(): void {
     if (this.formErrorControlName) {
-      const control = this.controlContainer.control?.get(this.formErrorControlName)
-      if (control)
-        this.setFormErros(control)
+      const control = this.controlContainer.control?.get(
+        this.formErrorControlName
+      );
+      if (control) this.setFormErros(control);
     }
   }
 
-
   setFormErros(control: FormControl | AbstractControl) {
     control.valueChanges.pipe(startWith(control.value)).subscribe(() => {
-      this.validateErrors(control.errors)
-    })
+      this.validateErrors(control.errors);
+    });
   }
 
   validateErrors(erros: ValidationErrors | null) {
-    // if (!erros) this.viewContainerRef.clear()
-    // if (this.elementRef)
-    //   this.viewContainerRef.createEmbeddedView(this.elementRef.nativeElement)
+    if (!erros) {
+      this.innerHtml = '';
+      return;
+    }
 
+    Object.keys(erros).forEach((key) => {
+      this.innerHtml = this.getError(key)();
+    });
+  }
+
+  getError(key: string) {
+    return ERROR_MESSAGES[key];
   }
 }
