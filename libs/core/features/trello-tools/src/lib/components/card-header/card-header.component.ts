d16cab1e9633fb6e0841a462047233e2a8f97475
pedro.henrique <pedro.henrique@brunsker.com.br>
2023-09-11 12:25:21 -0300

    Feat: card length

--- libs/core/features/trello-tools/src/lib/components/card-header/card-header.component.ts
+++ libs/core/features/trello-tools/src/lib/components/card-header/card-header.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'trello-card-header',
   templateUrl: './card-header.component.html',
   styleUrls: ['./card-header.component.scss'],
 })
-export class CardHeaderComponent implements OnInit {
-  constructor() {}
+export class CardHeaderComponent {
+  @Input() cardLength = 0
 
-  ngOnInit() {}
 }
