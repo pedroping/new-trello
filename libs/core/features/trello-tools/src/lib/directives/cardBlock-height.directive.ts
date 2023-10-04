0f8fbada2c4eb30ae23b5f009374f3a48c7920e1
pedroping <pedrpchavesdias@gmail.com>
2023-10-03 21:10:24 -0300

    Fix: call footer top on directive

--- libs/core/features/trello-tools/src/lib/directives/cardBlock-height.directive.ts
+++ libs/core/features/trello-tools/src/lib/directives/cardBlock-height.directive.ts
@@ -1,36 +1,36 @@
-import {
-  ContentChildren,
-  Directive,
-  HostBinding,
-  Input,
-  QueryList
-} from '@angular/core';
+import { Directive, HostBinding, Input } from '@angular/core';
 import { DragAndDropService } from '../services/drag-and-drop/drag-and-drop.service';
 
 @Directive({
   selector: '[trelloCardBlockHeight]',
+  exportAs: 'trelloCardBlockHeight',
 })
 export class CardBlockHeightDirective {
-  constructor(
-    private readonly dragAndDropService: DragAndDropService
-  ) { }
+  constructor(private readonly dragAndDropService: DragAndDropService) {}
 
   @Input() baseSize!: number;
   @Input() isSelected = false;
   @Input({ required: true }) type?: 'card' | 'block';
   @Input('trelloCardBlockHeight') length!: number;
 
-  @ContentChildren('card') cards?: QueryList<HTMLDivElement>;
+  footerTop: number = 0;
 
   @HostBinding('style.height') get cardHeight() {
-    if (
-      this.dragAndDropService.onCardMove$.value &&
-      !this.isSelected
-    ) {
+    this.setFooterTop();
+    if (this.dragAndDropService.onCardMove$.value && !this.isSelected) {
       const calcedHeight = (this.length + 1) * 40 + this.baseSize;
       return calcedHeight + 'px';
     }
     const calcedHeight = this.length * 40 + this.baseSize;
     return calcedHeight + 'px';
+  }
+
+  setFooterTop() {
+    const hasExpand =
+      this.dragAndDropService.onCardMove$.value && !this.isSelected;
+    const baseTop = this.length * 40 + (hasExpand ? 40 : 0);
+    const maxTop = window.innerHeight * 0.7;
+
+    this.footerTop = Math.min(baseTop, maxTop);
   }
 }
