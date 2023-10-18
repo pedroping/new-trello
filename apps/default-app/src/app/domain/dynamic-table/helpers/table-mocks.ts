4ad2185c0bb03ed037a8b939d0416918772b69d7
pedro.henrique <pedro.henrique@brunsker.com.br>
2023-10-18 10:21:18 -0300

    Refactor: remove useless piece of code

--- apps/default-app/src/app/domain/dynamic-table/helpers/table-mocks.ts
+++ apps/default-app/src/app/domain/dynamic-table/helpers/table-mocks.ts
@@ -16,17 +16,29 @@ export const DATA = Array.from({ length: 10 }, (_, index) => {
   };
 }) as IBasicTableTest[];
 
+export function createValueChangesFn(component: IBaseTableFather<IBasicTableTest>) {
+  const valueChangesFn = (
+    valueChanges$: Observable<IBasicTableTest>,
+    id: number,
+    element: IBasicTableTest,
+    selector: keyof IBasicTableTest
+  ) => {
+    component.getValueChanges(valueChanges$, id, element, selector);
+  }
+  return valueChangesFn
+}
+
 export const CREATE_TABLE_CONFIG = (
   component: IBaseTableFather<IBasicTableTest>
 ) => {
   return {
     hasExpansion: true,
-    hasPaginator: false,
+    hasPaginator: true,
     paginatorOptions: {
       pageSize: 5,
       pageSizeOptions: [2, 5, 10, 50, 100],
     },
-    hasDefaultPaginator: true,
+    hasDefaultPaginator: false,
     defaultPaginatorOptions: {
       totalSize: DATA.length,
       currentPage: 1,
@@ -49,14 +61,7 @@ export const CREATE_TABLE_CONFIG = (
         component: BasicInputComponent,
         controlsOptions: {
           controls: [],
-          getValueChanges: (
-            valueChanges$: Observable<IBasicTableTest>,
-            id: number,
-            element: IBasicTableTest,
-            selector: keyof IBasicTableTest
-          ) => {
-            component.getValueChanges(valueChanges$, id, element, selector);
-          },
+          getValueChanges: createValueChangesFn(component)
         },
       },
       {
