dbe8defcc47462e999ded375bc3feca484e98266
pedroping <pedrpchavesdias@gmail.com>
2023-11-15 13:58:55 -0300

    Feat: seo meta tags

--- apps/trello-clone/src/app/app.component.ts
+++ apps/trello-clone/src/app/app.component.ts
@@ -1,18 +1,20 @@
-import { Component, Injector } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component } from '@angular/core';
 import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
 import { MatChipsModule } from '@angular/material/chips';
-import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify';
+import { RouterModule } from '@angular/router';
+import { CoreFeaturesCustomBackgroundModule } from '@my-monorepo/core/features/custom-background';
+import { CoreFeaturesDragScrollModule } from '@my-monorepo/core/features/drag-scroll';
+import { CoreUiToolbarModule } from '@my-monorepo/core/ui/toolbar';
 import {
+  TUI_SANITIZER,
   TuiAlertModule,
   TuiDialogModule,
   TuiRootModule,
-  TUI_SANITIZER,
 } from '@taiga-ui/core';
 import { TuiAccordionModule } from '@taiga-ui/kit';
-import { CoreUiToolbarModule } from '@my-monorepo/core/ui/toolbar';
-import { CoreFeaturesDragScrollModule } from '@my-monorepo/core/features/drag-scroll';
-import { CoreFeaturesCustomBackgroundModule } from '@my-monorepo/core/features/custom-background';
+import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify';
+import { Meta } from '@angular/platform-browser';
+import { META_TAGS } from './shared/meta.tags';
 @Component({
   standalone: true,
   imports: [
@@ -32,4 +34,8 @@ import { CoreFeaturesCustomBackgroundModule } from '@my-monorepo/core/features/c
   styleUrls: ['./app.component.scss'],
   providers: [{ provide: TUI_SANITIZER, useClass: NgDompurifySanitizer }],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private meta: Meta) {
+    this.meta.addTags(META_TAGS);
+  }
+}
