155c8a400fbb2d6746e943ecbc54b93c2ba1320c
pedroping <pedrpchavesdias@gmail.com>
2023-11-21 21:45:37 -0300

    Feat: backdrop screen lib

---
+++ libs/core/features/backdrop-screen/src/lib/components/backdrop-screen/backdrop-screen.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-backdrop-screen',
+  templateUrl: './backdrop-screen.component.html',
+  styleUrls: ['./backdrop-screen.component.scss']
+})
+export class BackdropScreenComponent  {
+
+
+}
