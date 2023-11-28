6e065bd171eeebe95307ecc5c6593149b01ac23d
pedro.henrique <pedro.henrique@brunsker.com.br>
2023-11-28 18:16:11 -0300

    Feat: card edit

--- libs/core/features/trello-tools/src/lib/components/card-edit/card-edit.component.ts
+++ libs/core/features/trello-tools/src/lib/components/card-edit/card-edit.component.ts
@@ -2,14 +2,21 @@ import { Component, Input, OnInit } from '@angular/core';
 import { BackdropStateService } from '@my-monorepo/core/features/backdrop-screen';
 import { OutsideClickEventsService } from '@my-monorepo/core/utlis';
 import { Icard } from '../../models/card.models';
+import { CallSetValueChanges } from '@my-monorepo/core/features/set-value-changes-decorator';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'card-edit',
   templateUrl: './card-edit.component.html',
   styleUrls: ['./card-edit.component.scss'],
 })
+@CallSetValueChanges()
 export class CardEditComponent implements OnInit {
   @Input({ required: true }) card?: Icard;
+  cardNameControl = new FormControl('', {
+    nonNullable: true,
+    validators: [Validators.required],
+  });
 
   constructor(
     private readonly outsideClickEventsService: OutsideClickEventsService,
@@ -17,7 +24,8 @@ export class CardEditComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.setValueChanges();
+    if (!this.card) return;
+    this.cardNameControl.setValue(this.card.name);
   }
 
   setValueChanges() {
@@ -26,5 +34,14 @@ export class CardEditComponent implements OnInit {
     outSideClick$$.subscribe(() => {
       this.backdropStateService.setBackDropState(null);
     });
+
+    this.cardNameControl.valueChanges.subscribe((val) => {
+      if (!this.card) return;
+      this.card.name = val;
+    });
+  }
+
+  addCard() {
+    this.cardNameControl;
   }
 }
