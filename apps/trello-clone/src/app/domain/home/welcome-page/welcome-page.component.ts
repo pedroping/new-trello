8689286c89921ee8477b79239f744aa6cbb3cf25
pedro.henrique <pedro.henrique@brunsker.com.br>
2023-12-27 14:30:18 -0300

    Fix: set sidnav state to false

--- apps/trello-clone/src/app/domain/home/welcome-page/welcome-page.component.ts
+++ apps/trello-clone/src/app/domain/home/welcome-page/welcome-page.component.ts
@@ -38,7 +38,7 @@ export class WelcomePageComponent implements OnInit {
     this.genericSidenavsFacadeService.startDomain(
       this.pageContent.nativeElement
     );
-    this.genericSidenavsFacadeService.setLeftSideNavState(true);
-    this.genericSidenavsFacadeService.setRightSideNavState(true);
+    this.genericSidenavsFacadeService.setLeftSideNavState(false);
+    this.genericSidenavsFacadeService.setRightSideNavState(false);
   }
 }
