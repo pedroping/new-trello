9f9b57517341f17a27f79cc24d2755625d810054
pedro.henrique <pedro.henrique@brunsker.com.br>
2024-01-31 14:20:49 -0300

    Fix: variable scroll move value

--- libs/core/features/drag-scroll/src/lib/models/values.ts
+++ libs/core/features/drag-scroll/src/lib/models/values.ts
@@ -1,6 +1,6 @@
 export const BASE_BLOCK_SIZE = 320;
-export const BASE_SIDENAV_SIZE = 350;
 export const BASE_ADD_NEW_SIZE = 340;
-export const BASE_SCROLL_AREA = 200;
+export const BASE_SIDENAV_SIZE = 300 + window.innerWidth * 0.1;
+export const BASE_SCROLL_AREA = window.innerWidth * 0.2;
 export const BASE_SCROLL_MOVE_TICK = 2;
 export const LEFT_SIDENAV_GAP = 250;
