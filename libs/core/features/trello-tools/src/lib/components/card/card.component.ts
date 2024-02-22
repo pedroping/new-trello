99a7dd7f04df7c6fd063d5d6e6f75bfa288acb47
pedroping <pedrpchavesdias@gmail.com>
2024-02-22 13:12:46 -0300

    Feat: add scroll at card-list

--- libs/core/features/trello-tools/src/lib/components/card/card.component.ts
+++ libs/core/features/trello-tools/src/lib/components/card/card.component.ts
@@ -2,11 +2,11 @@ import {
   Component,
   ElementRef,
   Input,
-  TemplateRef,
-  ViewChild,
+  ViewChild
 } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import {
+  BackDropEvent,
   BackdropContentDirective,
   BackdropStateService,
 } from '@my-monorepo/core/features/backdrop-screen';
@@ -15,7 +15,6 @@ import { OutsideClickEventsService } from '@my-monorepo/core/utlis';
 import { merge, skip } from 'rxjs';
 import { CardEventsFacadeService } from '../../facades/card-events-facade.service';
 import { IBlock, Icard } from '../../models/card.models';
-import { BackDropEvent } from 'libs/core/features/backdrop-screen/src/lib/models/backdrop-screen-models';
 
 @Component({
   selector: 'trello-card',
