9f39ceee45092535d70a616bcc207835d513a60e
pedro.henrique <pedro.henrique@brunsker.com.br>
2024-02-28 10:05:44 -0300

    Fix: remove scss variables

--- apps/new-trello/src/app/app.component.ts
+++ apps/new-trello/src/app/app.component.ts
@@ -1,19 +1,10 @@
 import { Component } from '@angular/core';
-import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
-import { MatChipsModule } from '@angular/material/chips';
 import { RouterModule } from '@angular/router';
 import { CoreFeaturesBackdropScreenModule } from '@my-monorepo/core/features/backdrop-screen';
 import { CoreFeaturesCustomBackgroundModule } from '@my-monorepo/core/features/custom-background';
 import { CoreFeaturesDragScrollModule } from '@my-monorepo/core/features/drag-scroll';
 import { CoreUiToolbarModule } from '@my-monorepo/core/ui/toolbar';
-import {
-  TUI_SANITIZER,
-  TuiAlertModule,
-  TuiDialogModule,
-  TuiRootModule,
-} from '@taiga-ui/core';
-import { TuiAccordionModule } from '@taiga-ui/kit';
-import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify';
+
 @Component({
   selector: 'my-monorepo-root',
   templateUrl: './app.component.html',
@@ -21,17 +12,10 @@ import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify';
   standalone: true,
   imports: [
     RouterModule,
-    TuiRootModule,
-    MatChipsModule,
-    TuiAlertModule,
-    TuiDialogModule,
-    TuiAccordionModule,
     CoreUiToolbarModule,
-    MatBottomSheetModule,
     CoreFeaturesDragScrollModule,
     CoreFeaturesBackdropScreenModule,
     CoreFeaturesCustomBackgroundModule,
   ],
-  providers: [{ provide: TUI_SANITIZER, useClass: NgDompurifySanitizer }],
 })
 export class AppComponent {}
