2992e82526253ee30a2c64e416291e445cc09774
pedro.henrique <pedro.henrique@brunsker.com.br>
2024-03-01 15:33:35 -0300

    Fix: npm install conflicts

--- libs/core/features/drag-scroll/src/lib/models/values.ts
+++ libs/core/features/drag-scroll/src/lib/models/values.ts
@@ -1,6 +1,9 @@
 export const BASE_BLOCK_SIZE = 320;
 export const BASE_ADD_NEW_SIZE = 340;
 export const BASE_SIDENAV_SIZE = 300 + window.innerWidth * 0.15;
-export const BASE_SCROLL_AREA = Math.max(window.innerWidth * 0.2, 200);
+export const BASE_SCROLL_AREA = Math.min(
+  Math.max(window.innerWidth * 0.2, 200),
+  300,
+);
 export const BASE_SCROLL_MOVE_TICK = 5;
 export const LEFT_SIDENAV_GAP = 250;
